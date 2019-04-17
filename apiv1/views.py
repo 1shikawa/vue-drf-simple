5ddcef95c392d242ab3747c0ac71031a54a566e6
1shikawa <ishikawa.toru@gmail.com>
2019-04-17 12:25:48 +0900

    2019/04/17 initial commit

---
+++ apiv1/views.py
@@ -0,0 +1,13 @@
+from rest_framework import viewsets
+from rest_framework.permissions import IsAuthenticatedOrReadOnly
+
+from book.models import Book
+from .serializers import BookSerializer
+
+# Create your views here.
+class BookViewSet(viewsets.ModelViewSet):
+    '''BookオブジェクトのCRUDを行うAPI'''
+
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+    permission_classes = (IsAuthenticatedOrReadOnly, )
